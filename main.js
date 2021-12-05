72af071d715f0e22ea8b6eca7aed1dd97770734d
ArtyomLobanok <hejlenoctb3@gmail.com>
2021-12-05 11:37:47 +0300

    I think let test = 3

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-let user = "John Doe"
-console.log(user)
-let student = "Lobanok Artsiom"
-console.log(student)
-let student = user
+let user = "John Doe";
+console.log(user);
+let student = "Lobanok Artsiom";
 console.log(student);
+student +=user;
+console.log(user);
+
+//2 stage ht
+let test = 1;
+test = test++;
+test = +test;
+
