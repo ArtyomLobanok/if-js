3f773104e55ce026fc42aa7aaa9f12bc315672cf
ArtyomLobanok <hejlenoctb3@gmail.com>
2022-01-31 16:16:48 +0300

    add try catch ht 12

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 // lesson-12
 const getResponse = async () => {
-    const response = await fetch('https://fe-student-api.herokuapp.com/api/hotels/popular');
-    const content = await response.json();
-    console.log(content)
-    const list = document.getElementById('overviewsItems')
-    let key;
-    for (key in content) {
-        list.innerHTML += (`<div class="overviews__item">
+    console.log('Fetch todo started...')
+    try {
+        const response = await fetch('https://fe-student-api.herokuapp.com/api/hotels/popular');
+        const content = await response.json();
+        console.log(content)
+        const list = document.getElementById('overviewsItems')
+        let key;
+        for (key in content) {
+            list.innerHTML += (`<div class="overviews__item">
      <div class="overviews__img">
      <img src="${content[key].imageUrl}" alt="Pictures">
      </div>
      <div class="overviews__tittle">${content[key].name}</div>
      <div class="overviews__location">${content[key].city}, ${content[key].country}</div>
      </div>`)
+        }
+    } catch (e) {
+    console.error(e)
+    } finally {
     }
 }
 getResponse()
