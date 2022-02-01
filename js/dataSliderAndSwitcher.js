f23abfb68fbcfa626b0e302ada05722280abfaa3
ArtyomLobanok <hejlenoctb3@gmail.com>
2022-02-01 09:34:11 +0300

    ref code sliders && compleate ht 12

--- js/dataSliderAndSwitcher.js
+++ js/dataSliderAndSwitcher.js
@@ -1,142 +1,105 @@
-/*Carousel overviews responsive*/
-$(function () {
-
-    $('.overviews__items').slick({
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        speed: 1000,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
+/*Tabs destinations*/
+const tabDestinations = () => {
+    const tabTriggersItems = document.querySelectorAll('.tabs-triggers__item');
+    const tabsContentItems = document.querySelectorAll('.tabs-content__item');
+const startTab = () => {
+    $('.tabs-triggers__item').on('click', () => {
+        $('.destinations__slider').slick('unslick');
+        $('.destinations__slider').slick({
+            infinite: true,
+            draggable: false,
+            swipe: false,
+            speed: 1000,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            arrows: false,
+            variableWidth: false,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        arrows: true,
+                        draggable: true,
+                        swipe: true,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        arrows: true,
+                        draggable: true,
+                        swipe: true,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        arrows: true,
+                        draggable: true,
+                        swipe: true,
+                    }
+                },
+                {
+                    breakpoint: 360,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        arrows: true,
+                        draggable: true,
+                        swipe: true,
+                    }
                 }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 360,
-                settings: {
-                    slidesToShow: 2,
-                }
-            }
-        ]
-    });
-
-     /*Tab destinations*/
-     document.querySelectorAll('.tabs-triggers__item').forEach((item) =>
-     item.addEventListener('click', function (e) {
-         e.preventDefault();
-         const id = e.target.getAttribute('href').replace('#', '');
-
-         document.querySelectorAll('.tabs-triggers__item').forEach(
-             (child) => child.classList.remove('tabs-triggers__item--active')
-         );
-
-         document.querySelectorAll('.tabs-content__item').forEach(
-             (child) => child.classList.remove('tabs-content__item--active')
-         );
-
-         item.classList.add('tabs-triggers__item--active');
-         document.getElementById(id).classList.add('tabs-content__item--active');
-     })
-
- );
-
-
- $('.tabs-triggers__item').on('click', function () {
-     $('.destinations__slider').slick('unslick');
-     $('.destinations__slider').slick({
-         infinite: true,
-         draggable: false,
-         swipe: false,
-         speed: 1000,
-         slidesToShow: 4,
-         slidesToScroll: 1,
-         arrows: false,
-         variableWidth: false,
-         responsive: [
-             {
-                 breakpoint: 1024,
-                 settings: {
-                     slidesToShow: 3,
-                     slidesToScroll: 1,
-                     arrows: true,
-                     draggable: true,
-                     swipe: true,
-                 }
-             },
-             {
-                 breakpoint: 768,
-                 settings: {
-                     slidesToShow: 2,
-                     slidesToScroll: 1,
-                     arrows: true,
-                     draggable: true,
-                     swipe: true,
-                 }
-             },
-             {
-                 breakpoint: 480,
-                 settings: {
-                     slidesToShow: 2,
-                     slidesToScroll: 1,
-                     arrows: true,
-                     draggable: true,
-                     swipe: true,
-                 }
-             },
-             {
-                 breakpoint: 360,
-                 settings: {
-                     slidesToShow: 2,
-                     slidesToScroll: 1,
-                     arrows: true,
-                     draggable: true,
-                     swipe: true,
-                 }
-             }
-         ]
-     });
- });
-
- document.querySelector('.tabs-triggers__item').click();
-
-
-    /*Collapse destinations*/
-    $(document).ready(function () {
-        $(".destinations__collapse").click(function () {
-            $(".destinations__wrapper__second").toggleClass("active");
-            return false;
+            ]
         });
     });
-    $(document).ready(function () {
-        $(".destinations__collapse").click(function () {
-            $(".destinations__collapse").toggleClass("active");
-            return false;
-        });
-    });
-    $(document).ready(function () {
-        $(".destinations__collapse").click(function () {
-            $(".destinations__arrow").toggleClass("active");
-            return false;
-        });
-    });
+    document.querySelector('.tabs-triggers__item').click();
+}
+    tabTriggersItems.forEach((item) =>
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+            const changeClass = event.target.getAttribute('href').replace('#', '');
+            tabTriggersItems.forEach(
+                (child) => child.classList.remove('tabs-triggers__item--active')
+            );
+            tabsContentItems.forEach(
+                (child) => child.classList.remove('tabs-content__item--active')
+            );
+            item.classList.add('tabs-triggers__item--active');
+            document.getElementById(changeClass).classList.add('tabs-content__item--active');
+        })
+    )
+    startTab()
+};
+tabDestinations()
 
+/*Collapse destinations*/
+$(document).ready(() => {
+    $(".destinations__collapse").click(() => {
+        $(".destinations__wrapper__second").toggleClass("active");
+        return false;
+    });
+});
+$(document).ready(() => {
+    $(".destinations__collapse").click(() =>{
+        $(".destinations__collapse").toggleClass("active");
+        return false;
+    });
+});
+$(document).ready(() => {
+    $(".destinations__collapse").click(() => {
+        $(".destinations__arrow").toggleClass("active");
+        return false;
+    });
 });
 
-/*Carousel Reviews responsive*/
+/*Carousel destinationsCarousel responsive*/
+const destinationsCarousel = () => {
     $('.destinations__slider').slick({
         infinite: true,
         draggable: false,
@@ -188,9 +151,12 @@ $(function () {
                 }
             }
         ]
-    });
+    })
+};
+destinationsCarousel()
 
-
+/*Carousel Reviews responsive*/
+const reviewsCarousel = () => {
 $('.reviews__block').slick({
     dots: false,
     infinite: true,
@@ -239,9 +205,11 @@ $('.reviews__block').slick({
         }
     ]
 })
-    .on('setPosition', function (event, slick) {
+    .on('setPosition', (event, slick) => {
         slick.$slides.css('height', slick.$slideTrack.height() + 'px');
-    });
+    })
+};
+reviewsCarousel()
 
 
 
--- script.js
+++ script.js
@@ -1,86 +1,65 @@
 // lesson-12
+/*Carousel overviews responsive*/
+const startCarousel = () => {
+    $(function () {
+        $('.overviews__slider').slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            speed: 1000,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 360,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                }
+            ]
+        }).on('setPosition', function (event, slick) {
+            slick.$slides.css('height', slick.$slideTrack.height() + 'px');
+        })})};
+
 const getResponse = async () => {
-    console.log('Fetch todo started...')
+    console.log('Fetch todo started...');
     try {
         const response = await fetch('https://fe-student-api.herokuapp.com/api/hotels/popular');
         const content = await response.json();
-        console.log(content)
-        const list = document.getElementById('overviewsItems')
+        console.log(content);
+        const list = document.getElementById('overviewsItems');
         let key;
         for (key in content) {
             list.innerHTML += (`
             <div class="overviews__item">
-            <div class="overviews__img"></div>
-    <img src="${content[key].imageUrl}" alt="Pictures">
-    </div>
-    <div class="overviews__tittle">${content[key].name}</div>
-    <div class="overviews__location">${content[key].city}, ${content[key].country}</div>
-    </div>`)
+            <div class="overviews__img">
+                <img src="${content[key].imageUrl}" alt="Pictures">
+            </div>
+            <div class="overviews__tittle">${content[key].name}</div>
+            <div class="overviews__location">${content[key].city}, ${content[key].country}</div>
+            </div>`);
         }
+        startCarousel();
     } catch (e) {
-        console.error(e)
+        console.error(e);
     } finally {
     }
 }
-getResponse()
+getResponse();
 
-/*
-//lesson-10
-let itemsOut = document.querySelector('.overviews__items');
-const data = [
-    {
-        name: 'Hotel Leopold',
-        city: 'Saint Petersburg',
-        country: 'Russia',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-leopold_mflelk.jpg',
-    },
-    {
-        name: 'Apartment Sunshine',
-        city: 'Santa  Cruz de Tenerife',
-        country: 'Spain',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/apartment-sunshine_vhdlel.jpg',
-    },
-    {
-        name: 'Villa Kunerad',
-        city: 'Vysokie Tatry',
-        country: 'Slowakia',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/villa-kunerad_gdbqgv.jpg',
-    },
-    {
-        name: 'Hostel Friendship',
-        city: 'Berlin',
-        country: 'Germany',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/hostel-friendship_aw6tn7.jpg',
-    },
-    {
-        name: 'Radisson Blu Hotel',
-        city: 'Kyiv',
-        country: 'Ukraine',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/radisson-blu-hotel_jwtowg.jpg',
-    },
-    {
-        name: 'Paradise Hotel',
-        city: 'Guadalupe',
-        country: 'Mexico',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/paradise-hotel_i6whae.jpg',
-    },
-    {
-        name: 'Hotel Grindewald',
-        city: 'Interlaken',
-        country: 'Switzerland',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-grindewald_zsjsmy.jpg',
-    },
-    {
-        name: 'The Andaman Resort',
-        city: 'Port Dickson',
-        country: 'Malaysia',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
-    },
-];
-
-document.getElementById('overviewsItems').innerHTML = data.map(i =>
-    `<div class="overviews__item">
-    <img class="overviews__img" src="${i.imageUrl}" alt="Pictures">
-    <div class="overviews__tittle">${i.name}</div>
-    <div class="overviews__location">${i.city}, ${i.country}</div>
-    </div>`).join('');*/
