9ad8f18caa321fde96f0b971c6fbcba701d3ddd9
Артём Лобанок <91745257+ArtyomLobanok@users.noreply.github.com>
2022-02-03 17:40:51 +0300

    11,12.1+2 (#14)
    * processing ht 12
    
    * add try catch ht 12
    
    * test
    
    * ref code sliders && compleate ht 12
    
    * processing ht 11,12.2
    
    * processing
    
    * working on modal window close/open
    
    * compleate ht 11,12 but not compleate responseve

--- script.js
+++ script.js
@@ -1,59 +1,204 @@
-//lesson-10
-let itemsOut = document.querySelector('.overviews__items');
-const data = [
-    {
-        name: 'Hotel Leopold',
-        city: 'Saint Petersburg',
-        country: 'Russia',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-leopold_mflelk.jpg',
-    },
-    {
-        name: 'Apartment Sunshine',
-        city: 'Santa  Cruz de Tenerife',
-        country: 'Spain',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/apartment-sunshine_vhdlel.jpg',
-    },
-    {
-        name: 'Villa Kunerad',
-        city: 'Vysokie Tatry',
-        country: 'Slowakia',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/villa-kunerad_gdbqgv.jpg',
-    },
-    {
-        name: 'Hostel Friendship',
-        city: 'Berlin',
-        country: 'Germany',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/hostel-friendship_aw6tn7.jpg',
-    },
-    {
-        name: 'Radisson Blu Hotel',
-        city: 'Kyiv',
-        country: 'Ukraine',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/radisson-blu-hotel_jwtowg.jpg',
-    },
-    {
-        name: 'Paradise Hotel',
-        city: 'Guadalupe',
-        country: 'Mexico',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/paradise-hotel_i6whae.jpg',
-    },
-    {
-        name: 'Hotel Grindewald',
-        city: 'Interlaken',
-        country: 'Switzerland',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-grindewald_zsjsmy.jpg',
-    },
-    {
-        name: 'The Andaman Resort',
-        city: 'Port Dickson',
-        country: 'Malaysia',
-        imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
-    },
-];
+// lesson-12
+/*Carousel overviews responsive*/
+const startCarousel = () => {
+    $(function () {
+        $('.overviews__slider').slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            speed: 1000,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 360,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                }
+            ]
+        }).on('setPosition', function (event, slick) {
+            slick.$slides.css('height', slick.$slideTrack.height() + 'px');
+        })})};
 
-document.getElementById('overviewsItem').innerHTML = data.map(i =>
-    `<div class="overviews__item">
-    <img class="overviews__img" src="${i.imageUrl}" alt="Pictures">
-    <div class="overviews__tittle">${i.name}</div>
-    <div class="overviews__location">${i.city}, ${i.country}</div>
-    </div>`).join('');
+const getResponse = async () => {
+    console.log('Fetch todo started...');
+    try {
+        const response = await fetch('https://fe-student-api.herokuapp.com/api/hotels/popular');
+        const content = await response.json();
+        console.log(content);
+        const list = document.getElementById('overviewsItems');
+        let key;
+        for (key in content) {
+            list.innerHTML += (`
+            <div class="overviews__item">
+            <div class="overviews__img">
+                <img src="${content[key].imageUrl}" alt="Pictures">
+            </div>
+            <div class="overviews__tittle">${content[key].name}</div>
+            <div class="overviews__location">${content[key].city}, ${content[key].country}</div>
+            </div>`);
+        }
+        startCarousel();
+    } catch (e) {
+        console.error(e);
+    } finally {
+    }
+}
+getResponse();
+
+
+//lesson 11, 12-2;
+const mainForm = document.getElementById("mainForm");
+const inputs = mainForm.getElementsByClassName("thirdInputs");
+const childrenSelectors = document.getElementById("childrenSelectors");
+
+const inputsValidation = {
+
+    adults: {
+        min: 1,
+        max: 30,
+        defaultValue: 1,
+        counterElementId: "adultsCounter",
+    },
+    children: {
+        min: 0,
+        max: 10,
+        defaultValue: 0,
+        counterElementId: "childrenCounter",
+        additionalChanges: input => {
+            const { value } = input;
+            if (value > 0) {
+                document.getElementById("childrenSelectorsTitle").style.display = "block";
+            } else {
+                document.getElementById("childrenSelectorsTitle").style.display = "none";
+            }
+            if ({ value })
+            childrenSelectors.innerHTML = "";
+            for (let i = 0; i < value; i++) {
+                const newSelect = document.createElement('select');
+                newSelect.name = `select${i}`;
+                for (let j = 1; j < 18; j++) {
+                    newSelect.innerHTML += `<option value="${j}">${j} years old</option>`;
+                }
+                childrenSelectors.appendChild(newSelect);
+            }
+        },
+    },
+    rooms: {
+        min: 1,
+        max: 30,
+        defaultValue: 1,
+        counterElementId: "roomsCounter",
+    },
+};
+console.log(inputsValidation.adults.max)
+console.log(inputsValidation.children.max)
+console.log(inputsValidation.rooms.max)
+
+
+
+const inputDefaultInitialization = (input) => {
+    const { min, max, defaultValue, counterElementId, additionalChanges } = inputsValidation[input.name];
+    input.value = defaultValue;
+    const ElChang = () => {
+        counterElement.textContent = input.value;
+        if (additionalChanges) {
+            additionalChanges(input);
+        }}
+    const counterElement = document.getElementById(counterElementId);
+    counterElement.textContent = defaultValue;
+    const btns = Array.prototype.slice.call( input.closest(".numberChanger").getElementsByTagName("button") );
+    for (let btn of btns) {
+        if (btn.textContent === "-") {
+            btn.addEventListener('click', () => {
+                const plusBtn = btns.find(btn => btn.textContent === "+");
+                if (min < input.value) {
+                    input.value--;
+                    plusBtn.classList.add("btnActive");
+                }
+                if(input.value <= min) {
+                    btn.classList.remove("btnActive");
+                }
+                ElChang()
+            });
+        } else {
+            btn.addEventListener('click', () => {
+                const minusBtn = btns.find(btn => btn.textContent === "-");
+                if (max > input.value) {
+                    input.value++;
+                    minusBtn.classList.add("btnActive");
+                }
+                if(input.value >= max) {
+                    btn.classList.remove("btnActive");
+                }
+                ElChang()
+            });
+        }
+    }
+};
+
+for (let input of inputs) {
+    inputDefaultInitialization(input);
+}
+
+const getChildrenSelectorValues = () => {
+    const selectors = childrenSelectors.getElementsByTagName("select");
+    const childAgesData = [];
+    for (let selector of selectors) {
+        childAgesData.push(selector.value);
+    }
+    return childAgesData.toString();
+}
+
+/*Open/close Modal && add ActiveClass to imput */
+const mainMenu = document.getElementById('mainMenu');
+const menu = document.querySelector('.modalMenu');
+const toggleMenu = function() {
+    menu.classList.toggle('open');
+    mainMenu.classList.toggle('third__inputActive')
+}
+
+mainMenu.addEventListener('click', function(e) {
+    e.stopPropagation();
+    toggleMenu();
+});
+document.addEventListener('click', function(e) {
+    const target = e.target;
+    const its_menu = target == menu || menu.contains(target);
+    const its_mainMenu = target == mainMenu;
+    const menu_is_active = menu.classList.contains('open');
+
+    if (!its_menu && !its_mainMenu && menu_is_active) {
+        toggleMenu();
+    }
+});
+
+
+/*HT-12/2 */
+mainForm.onsubmit = async (event) => {
+    event.preventDefault();
+    const adults = event.target.adults.value;
+    const children = getChildrenSelectorValues();
+    const rooms = event.target.rooms.value;
+    const response = await fetch(`https://fe-student-api.herokuapp.com/api/hotels?search=us&adults=${adults}&children=${children}&rooms=${rooms}`)
+    const resJSON = await response.json();
+    console.log(resJSON);
+    console.log(getChildrenSelectorValues());
+}
