0a1fff07b41e7d3be7d0ce93a70c8e4227fccecb
ArtyomLobanok <hejlenoctb3@gmail.com>
2022-02-12 22:42:29 +0300

    copleate ht 14 with '*'

--- lesson-14/main.js
+++ lesson-14/main.js
@@ -1,13 +1,35 @@
 //lesson-14
     formElem.onsubmit = async (e) => {
     e.preventDefault();
-
     let response = await fetch('https://fe-student-api.herokuapp.com/api/file', {
     method: 'POST',
     body: new FormData(formElem)
 });
-
     let result = await response.json();
-
     console.log(result);
+};
+document.body.onload = addElement;
+let input = document.getElementById('inpFile');
+let prevImg = document.getElementById("myDiv");
+const previewImage = prevImg.querySelector('.image-preview__image');
+const previewDefaultText = prevImg.querySelector('.whatToDo');
+inpFile.addEventListener("change", function () {
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+
+        previewDefaultText.style.display = "none";
+        previewImage.style.display = "block";
+
+        reader.addEventListener("load", function () {
+            console.log(this);
+            previewImage.setAttribute("src", this.result);
+        });
+
+        reader.readAsDataURL(file);
+    };
+});
+function addElement() {
+    const triggerInput = () => input.click()
+    prevImg.addEventListener('click', triggerInput);
 };
