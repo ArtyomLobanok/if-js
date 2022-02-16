d6b115fcdf0e57caeb44f9f7812fb404aa319d48
ArtyomLobanok <hejlenoctb3@gmail.com>
2022-02-16 15:09:48 +0300

    compleate responseve

--- js/theCalendar.js
+++ js/theCalendar.js
@@ -4,7 +4,7 @@ dpMin = new AirDatepicker('#in_date', {
     autoClose: true,
     minDate: new Date(),
     view: 'days',
-    minView: 'months',
+    minView: 'days',
     dateFormat: 'E, MMM d',
     onSelect({date}) {
         dpMax.update({
@@ -17,7 +17,7 @@ dpMax = new AirDatepicker('#out_date', {
     autoClose: true,
     minDate: new Date(),
     view: 'days',
-    minView: 'months',
+    minView: 'days',
     dateFormat: 'E, MMM d',
     onSelect({date}) {
         dpMin.update({
