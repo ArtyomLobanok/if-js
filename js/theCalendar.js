27062333171e1c99a0c56477993b53c34c1589f5
ArtyomLobanok <hejlenoctb3@gmail.com>
2022-02-16 13:51:37 +0300

    add calendar but not compleate responseve

---
+++ js/theCalendar.js
@@ -0,0 +1,27 @@
+let dpMin, dpMax;
+
+dpMin = new AirDatepicker('#in_date', {
+    autoClose: true,
+    minDate: new Date(),
+    view: 'days',
+    minView: 'months',
+    dateFormat: 'E, MMM d',
+    onSelect({date}) {
+        dpMax.update({
+            minDate: date
+        })
+    },
+})
+
+dpMax = new AirDatepicker('#out_date', {
+    autoClose: true,
+    minDate: new Date(),
+    view: 'days',
+    minView: 'months',
+    dateFormat: 'E, MMM d',
+    onSelect({date}) {
+        dpMin.update({
+            maxDate: date
+        })
+    }
+});
